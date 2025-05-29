47e73d3bd4419db2cba1ed0b7a7a13b1a6cd2785
Nick Nisi <nick.nisi@workos.com>
2025-05-29 11:01:06 -0500

    add prettier

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  useTabs: false,
+  singleQuote: true,
+  printWidth: 120,
+  arrowParens: 'avoid',
+};
+
+export default config;
